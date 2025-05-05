1f27d4a469dfa323c74b8ae0b3ff3e9f66d03486
MrSobhan <sobhan.mosazadeh85@gmail.com>
2025-05-05 20:26:17 +0330

    Get Data in Table

--- src/components/data-table.jsx
+++ src/components/data-table.jsx
@@ -30,6 +30,9 @@ export function DataTable({ headers, data, onEdit, onDelete }) {
     pageSize: 10,
   })
 
+  console.log(data);
+  
+
 
   const columns = React.useMemo(
     () => [
@@ -132,26 +135,30 @@ export function DataTable({ headers, data, onEdit, onDelete }) {
           ))}
         </TableHeader>
         <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+          {
+            table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {
+                    row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </TableCell>
+                    ))
+                  }
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  بدون نتیجه
+                </TableCell>
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                بدون نتیجه
-              </TableCell>
-            </TableRow>
-          )}
+            )
+          }
         </TableBody>
       </Table>
       <div className="flex items-center justify-between px-4 py-2">
